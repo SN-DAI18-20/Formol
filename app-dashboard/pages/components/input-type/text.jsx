0abbdfee5a3caf5b979871d44d59cace246b9f6e
KamehaMehdi <dahmammehdi@gmail.com>
2020-03-19 17:44:32 +0100

    Adding text type input

--- app-dashboard/pages/components/input-type/text.jsx
+++ app-dashboard/pages/components/input-type/text.jsx
@@ -1,9 +1,52 @@
 import React from 'react';
 
+import { makeStyles } from '@material-ui/core/styles';
+
+import TextField from '@material-ui/core/TextField';
+import Select from '@material-ui/core/Select';
+import MenuItem from '@material-ui/core/MenuItem';
+
+const useStyle = makeStyles({
+	textStyle: {
+		display: 'flex',
+		justifyContent: 'baseline'
+	},
+	textField: {
+		marginRight: '50px'
+	}
+});
+
 export default () => {
-    return (
-        <div>
-          TEXT
-        </div>
-    )
-}
+	const [ text, setText ] = React.useState('');
+	const [ selectedTextType, setSelectedTextType ] = React.useState('Court');
+  const [ selectedType, setSelectedType ] = React.useState('text');
+
+  const { textField, textStyle } = useStyle();
+
+	return (
+		<div className={textStyle}>
+			<Select value={selectedType} onChange={({ target }) => setSelectedType(target.value)}>
+				<MenuItem value="text" children="text" />
+				<MenuItem value="email" children="email" />
+				<MenuItem value="tel" children="tel" />
+			</Select>
+			<TextField
+				className={textField}
+				label="Placeholder"
+				value={text}
+				onChange={({ currentTarget }) => {
+					setText(currentTarget.value);
+				}}
+			/>
+			<Select
+				value={selectedTextType}
+				onChange={({ target }) => {
+					setSelectedTextType(target.value);
+				}}
+			>
+				<MenuItem value="Court" children="Court" />
+				<MenuItem value="Long" children="Long" />
+			</Select>
+		</div>
+	);
+};
