b0a93d05efdea61d11b3f12bb857dcf6b5bfba79
KamehaMehdi <dahmammehdi@gmail.com>
2020-03-19 19:04:07 +0100

    Adding number type input

--- app-dashboard/pages/components/input-type/number.jsx
+++ app-dashboard/pages/components/input-type/number.jsx
@@ -1,9 +1,59 @@
 import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+
+import TextField from '@material-ui/core/TextField';
+import Switch from '@material-ui/core/Switch';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import FormControl from '@material-ui/core/FormControl';
+
+const useStyle = makeStyles({
+	number: {
+		display: 'flex',
+		flexDirection: 'row'
+	}
+});
 
 export default () => {
-    return (
-        <div>
-            NUMBER
-        </div>
-    )
-}
+	const { number } = useStyle();
+	const [ between, setBetween ] = React.useState(false);
+	const [ min, setMin ] = React.useState(0);
+	const [ max, setMax ] = React.useState();
+
+	React.useEffect(
+		() => {
+			setMin(0);
+		},
+		[ between ]
+	);
+
+	return (
+		<div>
+			<FormControlLabel
+				label="Between"
+				control={<Switch color="primary" onChange={() => setBetween(!between)} />}
+			/>
+			<div className={number}>
+				<React.Fragment>
+					{between && (
+						<FormControl>
+							<TextField
+								onChange={({ target }) => setMin(target.value)}
+								value={min}
+								label="De"
+								type="number"
+							/>
+						</FormControl>
+					)}
+					<FormControl>
+						<TextField
+							onChange={({ target }) => setMax(target.value)}
+							value={max}
+							label={between ? 'À' : 'Limite'}
+							type="number"
+						/>
+					</FormControl>
+				</React.Fragment>
+			</div>
+		</div>
+	);
+};
