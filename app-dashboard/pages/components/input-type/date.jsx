0c3fb46bc954cb73370c188e53014ab044a6da31
KamehaMehdi <dahmammehdi@gmail.com>
2020-03-22 17:18:28 +0100

    Adding date type input

--- app-dashboard/pages/components/input-type/date.jsx
+++ app-dashboard/pages/components/input-type/date.jsx
@@ -1,9 +1,66 @@
 import React from 'react';
 
+
+import Switch from '@material-ui/core/Switch';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import DateFnsUtils from '@date-io/date-fns';
+import { KeyboardDatePicker, MuiPickersUtilsProvider } from '@material-ui/pickers';
+
+import { makeStyles } from '@material-ui/core/styles';
+const useStyle = makeStyles({
+  minDateStyle: {
+    marginRight: '10px'
+  }
+})
+
 export default () => {
-  return (
-    <div>
-      DATE
-    </div>
-  )
-}
+
+  const { minDateStyle } = useStyle();
+
+	const [ maxDate, setMaxDate ] = React.useState();
+	const handleMaxDateChange = (maxDate) => {
+		setMaxDate(maxDate);
+	};
+
+	const [ minDate, setMinDate ] = React.useState();
+	const handleMinDateChange = (minDate) => {
+		setMinDate(minDate);
+  };
+
+  const [between, setBetween] = React.useState(false);
+  const handleToggleBetween = ({target}) => {
+    const {checked} = target
+    setBetween(checked)
+  }
+
+	return (
+		<div>
+			<FormControlLabel control={<Switch color="primary" onChange={handleToggleBetween} />} label="Between" />
+			<div>
+        {between && <MuiPickersUtilsProvider utils={DateFnsUtils}>
+        <KeyboardDatePicker
+            className={minDateStyle}
+						value={minDate}
+						onChange={handleMinDateChange}
+						disableToolbar
+						variant="inline"
+						format="MM/dd/yyyy"
+						margin="normal"
+						label="Min date"
+					/>
+        </MuiPickersUtilsProvider>}
+				<MuiPickersUtilsProvider utils={DateFnsUtils}>
+					<KeyboardDatePicker
+						value={maxDate}
+						onChange={handleMaxDateChange}
+						disableToolbar
+						variant="inline"
+						format="MM/dd/yyyy"
+						margin="normal"
+						label="Max date"
+					/>
+				</MuiPickersUtilsProvider>
+			</div>
+		</div>
+	);
+};
