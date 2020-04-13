661b9b44a79f968d50d9b35e6d9311921f89c3fe
KamehaMehdi <dahmammehdi@gmail.com>
2020-04-13 12:27:30 +0200

    Change formulaire page to create-formulaire page

---
+++ app-dashboard/pages/create-formulaire.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import Container from '@material-ui/core/Container';
+import Box from '@material-ui/core/Box';
+
+import Formulaire from '../src/components/formulaire/Formulaire';
+
+export default () => {
+
+    return (
+        <Container maxWidth="md">
+            <Box my={4}>
+              <Formulaire />
+            </Box>
+        </Container>
+    )
+}
