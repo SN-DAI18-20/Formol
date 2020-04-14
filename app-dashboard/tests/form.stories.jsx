51b33abf6bb3fad08b1224e9c7591fdc76440612
KamehaMehdi <dahmammehdi@gmail.com>
2020-04-14 23:07:36 +0200

    Update context state logic

---
+++ app-dashboard/tests/form.stories.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+import { Formulaire } from '../src/components/formulaire/Formulaire';
+
+export default {title: 'Formulaire'}
+
+export const FormulaireNoState = () => <Formulaire/>
