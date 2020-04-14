dc83d2ae45d4fe258cda17f9f2b7eea92bedd8b9
KamehaMehdi <dahmammehdi@gmail.com>
2020-04-14 23:07:36 +0200

    Update context state logic

--- app-dashboard/pages/modify-formulaire.jsx
+++ app-dashboard/pages/modify-formulaire.jsx
@@ -5,35 +5,14 @@ import { useRouter } from 'next/router'
 import Container from '@material-ui/core/Container';
 import Box from '@material-ui/core/Box';
 
-import { Questions } from '../src/components/formulaire/Questions';
-import { Header } from '../src/components/formulaire/Header';
-import { Draft } from '../src/components/formulaire/Draft';
-
-import { FormulaireContext, reducer } from '../src/utils/Contexts';
-import { Divider } from '@material-ui/core';
-
-import { makeStyles } from '@material-ui/core/styles';
-const useStyle = makeStyles({
-  dividerStyle:{
-    margin: "40px 0px"
-  }
-})
+import { Formulaire } from '../src/components/formulaire/Formulaire';
 
 export default () => {
-
-  const { dividerStyle } = useStyle();
-
-  const [state, dispatch] = React.useReducer(reducer);
 
   return (
     <Container maxWidth="md">
       <Box my={4}>
-        <FormulaireContext.Provider value={{state, dispatch}}>
-          <Header/>
-          <Questions/>
-          <Divider className={dividerStyle} />
-          <Draft/>
-        </FormulaireContext.Provider>
+        <Formulaire/>
       </Box>
     </Container>
   )
