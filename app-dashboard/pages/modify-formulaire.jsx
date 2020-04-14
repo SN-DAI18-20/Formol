5e37c399ddd3f6f6b474a28e7605d5a7549a61a8
KamehaMehdi <dahmammehdi@gmail.com>
2020-04-14 23:07:36 +0200

    Update context state logic

--- app-dashboard/pages/modify-formulaire.jsx
+++ app-dashboard/pages/modify-formulaire.jsx
@@ -5,31 +5,14 @@ import { useRouter } from 'next/router'
 import Container from '@material-ui/core/Container';
 import Box from '@material-ui/core/Box';
 
-import { getVersion } from '../src/utils/Requests';
-
 import { Formulaire } from '../src/components/formulaire/Formulaire';
 
 export default () => {
 
-  const router = useRouter()
-
-  const { pollId, id } = router.query
-  const [specificPollData, setSpecificPollData] = React.useState()
-
-  React.useEffect(() => {
-    (async () => {
-      if(id && pollId){
-        const specificPollData = await getVersion(pollId, id)
-        console.log({specificPollData})
-        setSpecificPollData(specificPollData)
-      }
-    })()
-  }, [router])
-
   return (
     <Container maxWidth="md">
       <Box my={4}>
-        <Formulaire modify={true} defaultData={specificPollData} />
+        <Formulaire/>
       </Box>
     </Container>
   )
