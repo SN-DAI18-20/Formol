635aae232847ae6451f2f5177403130e6aa81581
STan <stane.marchesan@gmail.com>
2020-04-20 14:46:51 +0200

    Ajout de la page affichant la liste des formulaires

---
+++ app-dashboard/pages/formulaires.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import Container from '@material-ui/core/Container';
+import ListPolls from '../src/components/pollsList';
+
+export default () => {
+    return (
+        <Container>
+            <ListPolls></ListPolls>
+        </Container>
+    );
+}
