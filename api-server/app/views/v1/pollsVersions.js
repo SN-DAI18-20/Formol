d4356ec67c8d1bc6c77dfdde776e1409c7b1b7c2
camelia <camelia2mazouzi@gmail.com>
2020-04-20 16:36:33 +0200

    Polls versions: creation des routes

--- api-server/app/views/v1/pollsVersions.js
+++ api-server/app/views/v1/pollsVersions.js
@@ -1,3 +1,9 @@
+const { sequelize } = require('../../models');
+const { Op } = require('sequelize');
+
+const { Polls, PollsVersions, PollsQuestions } = require('../../models');
+const generate_uid = require('../../helpers/uid-creation');
+const QuestionsValidators = require('./validators/questionTypes');
 const GenericSchema = require('./genericSchema');
 
 const PollVersionGenericSchema = {
@@ -181,16 +187,245 @@ async function VersionCollectionGet(request, reply) {
 async function VersionCollectionPost(request, reply) {
     const body = request.body;
     const validationErrors = [];
+
+    // Validate parameters for each question
+    request.body.form.forEach((el, it) => {
+        try {
+            const values = QuestionsValidators[el.type](el);
+        } catch (error) {
+            validationErrors.push([error.message, it]);
+        }
+    });
+
+    if (validationErrors.length != 0) {
+        let errorMessage = '';
+
+        validationErrors.forEach((el, it) => {
+            const msg = `body.form[${el[1]}].parameters.${el[0]}`;
+            errorMessage += it != 0 ? ', ' + msg : msg;
+        });
+
+        return reply.badRequest(errorMessage).sent();
+    }
+
+    // Beginning of the creation process
+    const transaction = await sequelize.transaction();
+    let poll_id = request.params.pollId;
+
+    try {
+        const version = await PollsVersions.create(
+            {
+                name: generate_uid(),
+                poll: poll_id,
+                active: true,
+            },
+            { transaction }
+        );
+
+        const questions = [];
+        body.form.forEach(async el => {
+            questions.push({
+                poll_version: version.id,
+                question: el.question,
+                type: el.type,
+                parameters: el.parameters,
+                required: el.required || false,
+            });
+        });
+
+        await PollsQuestions.bulkCreate(questions, { transaction });
+
+        await transaction.commit();
+    } catch (error) {
+        request.log.error(error);
+        request.log.trace('Unable to create the version');
+
+        await transaction.rollback();
+
+        return reply.internalServerError();
+    }
+
+    return reply.code(201).send(
+        JSON.stringify({
+            message: 'Ressource created',
+            ressource_id: poll_id,
+        })
+    );
 }
 
-async function VersionActiveGet(request, reply) {}
+async function VersionActiveGet(request, reply) {
+    const poll_id = request.params.pollId;
+    const questions = [];
+    const version = await PollsVersions.findOne({
+        where :{ [OP.and]: 
+        [{poll: poll_id}, 
+        {active: true}]
+        }
+        
+    });
+    if (version === null) {
+        return reply
+        .notFound(`Ressource '${request.params.pollId}' not exists.`)
+        .sent();
+    }
 
-async function VersionGet(request, reply) {}
+    const questionsQuerry = await PollsQuestions.findAll({
+        where : {
+            poll_version : version.id,
+        }
+    });
+    const questionMap = questionsQuerry.map(PollsQuestions => PollsQuestions.toJson());
+    questionMap.forEach(element => {
+        // const param = [];
+        // element.parameters.forEach(par =>{
+        //     param.push(par);
+        // });
+        questions.push({
+            question: element.question,
+            type: element.type,
+            parameters: element.parameters, 
+            required: element.required,
+        });
+    });
+    const result = {
+        id: version.id, 
+        name: version.name,
+        poll: poll_id,
+        questions: questions,
+        view_href: version.view_url,
+        download_href: version.download_url,
+    };
+    return reply.send(JSON.stringify(result));
+}
+
+async function VersionGet(request, reply) {
+    const poll_id = request.params.pollId;
+    const version_id = request.params.versionId;
+    const questions = [];
+    const version = await PollsVersions.findOne({
+        where :{ [OP.and]: 
+        [{poll: poll_id}, 
+        {id : version_id}]
+        }
+        
+    });
+    if (version === null) {
+        return reply
+        .notFound(`Ressource '${request.params.pollId}' not exists.`)
+        .sent();
+    }
+
+    const questionsQuerry = await PollsQuestions.findAll({
+        where : {
+            poll_version : version.id,
+        }
+    });
+    const questionMap = questionsQuerry.map(PollsQuestions => PollsQuestions.toJson());
+    questionMap.forEach(element => {
+        // const param = [];
+        // element.parameters.forEach(par =>{
+        //     param.push(par);
+        // });
+        questions.push({
+            question: element.question,
+            type: element.type,
+            parameters: element.parameters, 
+            required: element.required,
+        });
+    });
+    const result = {
+        id: version.id, 
+        name: version.name,
+        poll: poll_id,
+        questions: questions,
+        view_href: version.view_url,
+        download_href: version.download_url,
+    };
+    reply.send(JSON.stringify(result));
+}
 async function VersionActivePut(request, reply) {
     // This route doesn't have parameters in body. When the route is called,
     // we disable the current version and set the given version as active.
+    const pollId = request.params.pollId;
+    const versionId = request.params.versionId;
+    try{
+        const versionget = await PollsVersions.findOne({
+            where: { [OP.and]: 
+                [{poll: pollId}, 
+                {id : versionId}]
+            }
+        });
+        if (versionget == null){
+            return reply.notFound(`Ressource '${versionId}' not exists.`)
+        }
+        await PollsVersions.update({
+            active: true,},
+            {where:{ [OP.and]: 
+                [{poll: pollId}, 
+                {activate : true}]
+            },
+        })
+        .success(function(){
+            await PollsVersions.update({
+                active: true,},
+                {where:{ [OP.and]: 
+                    [{poll: pollId}, 
+                    {id : versionId}]
+                    }
+            })
+            .success(function(){
+                reply.code(200).send(JSON.stringify({ message: 'Request accepted' }));
+            });
+        });
+    }catch(error){
+        request.log.trace(error);
+        request.log.trace(`Unable to update poll '${versionId}'`);
+        return reply.internalServerError();
+    }  
+
 }
-async function VersionDelete(request, reply) {}
+async function VersionDelete(request, reply) {
+    const pollId = request.params.pollId;
+    const versionId = request.params.versionId;
+    const transaction = sequelize.transaction();
+    //Doit delete questions ? 
+    const versionget = await PollsVersions.findOne({
+        where: { [OP.and]: 
+            [{poll: pollId}, 
+            {id : versionId}]
+        }
+    });
+    if (versionget == null){
+        return reply.notFound(`Ressource '${versionId}' not exists.`)
+    }
+    try{
+        await PollsVersions.destroy(
+            {
+                where: { [OP.and]: 
+                    [{poll: pollId}, 
+                    {id : versionId}]
+                }
+            },
+            {transaction}
+        );
+//        await PollsQuestions.destroy(
+//            {
+//                where: {poll_version : versionId}
+//            },
+//            {transaction}
+//        );
+        await transaction.commit();
+    }catch(error){
+        request.log.trace(`Unable to delete version '${versionId}'`);
+
+        await transaction.rollback();
+
+        return reply.internalServerError();
+    }
+
+    return reply.code(200).send(JSON.stringify({ message: 'Request accepted' }));
+
+}
 
 module.exports = {
     polls: {
