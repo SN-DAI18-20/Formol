af1e6dbd9527ca5470261efe9063244abf30c933
KamehaMehdi <dahmammehdi@gmail.com>
2020-04-21 23:31:17 +0200

    Integrate layout and add login/signup pages

---
+++ app-dashboard/pages/signup.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+import Signup from '../src/components/signup'
+import { Layout } from '../src/components/Layout'
+
+export default () => {
+  return (
+    <Layout>
+      <Signup/>
+    </Layout>
+  )
+}
