7a6b04d9036401d8c5cdf3c344a7b890e930be78
KamehaMehdi <dahmammehdi@gmail.com>
2020-04-21 23:31:17 +0200

    Integrate layout and add login/signup pages

--- app-dashboard/pages/create-formulaire.jsx
+++ app-dashboard/pages/create-formulaire.jsx
@@ -3,15 +3,18 @@ import React from 'react';
 import Container from '@material-ui/core/Container';
 import Box from '@material-ui/core/Box';
 
+import { Layout } from '../src/components/Layout'
 import { Formulaire } from '../src/components/formulaire/Formulaire';
 
 export default () => {
 
     return (
+      <Layout>
         <Container maxWidth="md">
             <Box my={4}>
-              <Formulaire />
+                <Formulaire />
             </Box>
         </Container>
+      </Layout>
     )
 }
--- app-dashboard/pages/formulaires.jsx
+++ app-dashboard/pages/formulaires.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
 
+import { Layout } from '../src/components/Layout'
+
 import Container from '@material-ui/core/Container';
 import ListPolls from '../src/components/pollsList';
 
 export default () => {
     return (
+      <Layout>
         <Container>
             <ListPolls></ListPolls>
         </Container>
+      </Layout>
     );
 }
