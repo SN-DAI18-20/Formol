92abfcdf559937f9c0895682e2de55cc91623d3c
Kira <kirawattremez@gmail.com>
2020-04-23 18:49:27 +0200

    SendMail

--- api-server/app/mails/index.js
+++ api-server/app/mails/index.js
@@ -8,6 +8,25 @@ await function fillTemplate(template, data) {
 
 await function sendMail(to, template, data) {
 
+    const sgMail = require('@sendgrid/mail');
+    sgMail.setApi.apikey(process.env.sendGridApiKey);
+    const message = {
+        To : 'Test@test.com',
+        From : 'Test&test.com',
+        Subject : "Test Mail",
+        Text : "Test test truc, blabla, Non ?",
+        HTML : <strong> Questionnaire en ligne OK. </strong>
+    }
+    sgMail
+        .sendMail(message)
+        .then (() => {}, console.error);
+        (async() => {
+            try{
+                await sgMail.sendMail(message);
+            }catch (erreur){
+                console.error(erreur.toString());
+            }
+        })
 }
 
 module.exports = {
