f60a47f7585f54eef4082adc72ab4cce92faf89d
Kira <kirawattremez@gmail.com>
2020-04-23 18:49:27 +0200

    api-server: Write sendMail and fillTemplate methods

--- api-server/app/mails/index.js
+++ api-server/app/mails/index.js
@@ -1,16 +1,60 @@
+const sendgrid = require('@sendgrid/mail');
+
+const log = require('../server').log;
+
 const appConfig = require('../../config/server');
+const templates = require('./templates');
+
 const globalEmail = appConfig.email.globalEmail;
 const sendGridApiKey = appConfig.email.apikey;
 
-await function fillTemplate(template, data) {
 
+
+function fillTemplate(template, data) {
+    if(!Object.keys(templates).includes(template)) {
+        throw new Error(`Template '${template}' not exists.`);
+    }
+
+    let renderedSubject = templates[template].title;
+    let renderedTextBody = templates[template].text;
+    let renderedHTMLBody = templates[template].body;
+
+    for(key in data) {
+        renderedSubject = renderedSubject.replace(`$${key}`, data[key]);
+        renderedTextBody = renderedTextBody.replace(`$${key}`, data[key]);
+
+        if(renderedHTMLBody != null) {
+            renderedHTMLBody = renderedHTMLBody.replace(`$${key}`, data[key]);
+        }
+    }
+
+    return {
+        subject: renderedSubject,
+        text: renderedTextBody,
+        html: renderedHTMLBody,
+    };
 }
 
-await function sendMail(to, template, data) {
+async function sendMail(to, template, data) {
+    sendgrid.setApiKey(sendGridApiKey);
 
+    try {
+        const renderedTemplate = fillTemplate(template, data);
+
+        await sendgrid.send({
+            to: to,
+            from: globalEmail,
+            subject: renderedTemplate.subject,
+            text: renderedTemplate.text,
+            html: renderedTemplate.html,
+        });
+    } catch (err) {
+        log.error(`Unable to send mail to '${to}'. Traceback:`, err);
+        return Promise.reject();
+    }
 }
 
 module.exports = {
     fillTemplate,
-    sendMail
-}
+    sendMail,
+};
