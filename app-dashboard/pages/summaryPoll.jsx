c1764990a0bd2a1222bef3f0589183cb69b8e65a
STan <stane.marchesan@gmail.com>
2020-04-23 20:40:39 +0200

    Ajout de page pour la gestion du formualire

---
+++ app-dashboard/pages/summaryPoll.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+import Container from '@material-ui/core/Container';
+import SummaryPoll from '../src/components/pollSummary';
+
+export default () => {
+    return(
+        <SummaryPoll pollId={1}></SummaryPoll>
+    );
+}
